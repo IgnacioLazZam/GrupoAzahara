e4d050552bef9cb198fe6015ceaa708e882e55b1
IgnacioLazZam <ignacio.laz.zam@gmail.com>
2024-12-20 11:37:15 +0100

    side nav is closed when section is selected, added 3 more projects and a new section "in-process-projects" which doesnt have content.

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 function toggleMenu() {
     document.getElementById('navLinks').classList.toggle('active');
 }
+
+
+document.querySelectorAll('#navLinks a').forEach(item => {
+    item.addEventListener('click', () => {
+        document.getElementById('navLinks').classList.remove('active');
+    });
+});
