9b3d5cd18752ad5b175a8dbeb219a8481ec11e60
IgnacioLazZam <ignacio.laz.zam@gmail.com>
2025-11-16 18:32:36 +0100

    Adición de modal al clicar en las imágenes

--- script.js
+++ script.js
@@ -8,3 +8,30 @@ document.querySelectorAll('#navLinks a').forEach(item => {
         document.getElementById('navLinks').classList.remove('active');
     });
 });
+
+function openModal(img) {
+     if (window.innerWidth < 1000) {
+    return;
+  }
+  const modal = document.getElementById('imageModal');
+  const modalImg = document.getElementById('modalImage');
+  const caption = document.getElementById('modalCaption');
+  
+  modal.style.display = 'block';
+  modalImg.src = img.src;
+  caption.innerHTML = img.nextElementSibling.textContent;
+  
+  document.body.style.overflow = 'hidden';
+}
+
+function closeModal() {
+  const modal = document.getElementById('imageModal');
+  modal.style.display = 'none';
+  document.body.style.overflow = 'auto';
+}
+
+document.addEventListener('keydown', function(event) {
+  if (event.key === 'Escape') {
+    closeModal();
+  }
+});
